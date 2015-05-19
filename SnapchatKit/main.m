8920f321aac5e0c2cb615ea80a09373bcb932d4a
Tanner Bennett <tannerbennett@me.com>
2015-05-19 11:21:04 -0500

    Moved SKStoryNote to /Classes, oops

--- SnapchatKit/main.m
+++ SnapchatKit/main.m
@@ -23,6 +23,7 @@ int main(int argc, const char * argv[]) {
                 // For debugging purposes, to see the size of the response JSON in memory. Mine was about 300 KB.
                 // Probably quadratically larger though, since each object also holds onto its JSON dictionary,
                 // ie each SKStoryCollection has the JSON for each story, and each SKStory also has its JSON.
+                // TODO: make the _JSON property of SKThing dependent on kDebugJSON.
                 NSData *data = [NSPropertyListSerialization dataWithPropertyList:[session valueForKey:@"_JSON"] format:NSPropertyListBinaryFormat_v1_0 options:0 error:nil];
                 NSLog(@"Bytes: %lu Kilobytes: %f", (unsigned long)data.length, ((float)data.length/1024.f));
             }
