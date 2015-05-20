99d8743a1a9f1253aecf3da98723191860931586
Tanner Bennett <tannerbennett@me.com>
2015-05-20 18:08:13 -0500

    Finished up SKSession with the values in "updates_response"
    Mostly preferences and such

--- SnapchatKit/Classes/Model/SKSession.h
+++ SnapchatKit/Classes/Model/SKSession.h
@@ -9,6 +9,16 @@
 #import <Foundation/Foundation.h>
 #import "SKThing.h"
 
+typedef NS_ENUM(NSUInteger, SKStoryPrivacy)
+{
+    SKStoryPrivacyEveryone = 1,
+    SKStoryPrivacyFriends,
+    SKStoryPrivacyCustom
+};
+
+extern SKStoryPrivacy SKStoryPrivacyFromString(NSString *storyPrivacyString);
+
+
 @interface SKSession : SKThing //<NSCoding>
 
 + (instancetype)sessionWithJSONResponse:(NSDictionary *)json;
@@ -19,6 +29,8 @@
 @property (nonatomic, readonly) BOOL emailVerified;
 
 @property (nonatomic, readonly) NSString *backgroundFetchSecret;
+
+#pragma mark User data
 
 /** Array of SKUser objects. */
 @property (nonatomic, readonly) NSArray *friends;
@@ -36,7 +48,78 @@
 /** Array of SKStory objects of the user's group stories. Empty so far. */
 @property (nonatomic, readonly) NSArray *groupStories;
 
+#pragma mark Cash information
+@property (nonatomic, readonly) BOOL         canUseCash;
+@property (nonatomic, readonly) BOOL         isCashActive;
+/** Seems to be your username. */
+@property (nonatomic, readonly) NSString     *cashCustomerIdentifier;
+/** i.e. "SQUARE" */
+@property (nonatomic, readonly) NSString     *cashProvider;
+/** Keys: "snapcash_new_tos_accepted", "snapcash_tos_v2_accepted", "square_tos_accepted" */
+@property (nonatomic, readonly) NSDictionary *cashClientProperties;
 
-//@property (nonatomic, readonly) 
+#pragma mark Basic user information
+@property (nonatomic, readonly) NSString     *username;
+@property (nonatomic, readonly) NSString     *email;
+@property (nonatomic, readonly) NSString     *mobileNumber;
+@property (nonatomic, readonly) NSUInteger   recieved;
+@property (nonatomic, readonly) NSUInteger   sent;
+@property (nonatomic, readonly) NSUInteger   score;
+/** Array of usernames. */
+@property (nonatomic, readonly) NSArray      *recents;
+/** Probably an array of usernames. */
+@property (nonatomic, readonly) NSArray      *requests;
+
+
+#pragma mark Account information
+@property (nonatomic, readonly) NSDate       *addedFriendsTimestamp;
+@property (nonatomic, readonly) NSString     *authToken;
+@property (nonatomic, readonly) BOOL         canSeeMatureContent;
+/** i.e. "US" */
+@property (nonatomic, readonly) NSString     *countryCode;
+@property (nonatomic, readonly) NSDate       *lastTimestamp;
+@property (nonatomic, readonly) NSString     *devicetoken;
+@property (nonatomic, readonly) BOOL         canSaveStoryToGallery;
+/** Unknown. */
+@property (nonatomic, readonly) BOOL         canVideoTranscodingAndroid;
+@property (nonatomic, readonly) BOOL         imageCaption;
+/** Unknown. */
+@property (nonatomic, readonly) BOOL         isTwoFAEnabled;
+@property (nonatomic, readonly) NSDate       *lastAddressBookUpdateDate;
+@property (nonatomic, readonly) NSDate       *lastReplayedSnapDate;
+/** Unknown. */
+@property (nonatomic, readonly) BOOL         logged;
+@property (nonatomic, readonly) NSString     *mobileVerificationKey;
+@property (nonatomic, readonly) BOOL         canUploadRawThumbnail;
+/** Array of strings. */
+@property (nonatomic, readonly) NSArray      *seenTooltips;
+@property (nonatomic, readonly) BOOL         shouldCallToVerifyNumber;
+@property (nonatomic, readonly) BOOL         shouldTextToVerifyNumber;
+/** YES if everyone can send you snaps, NO if only friends can. */
+/** Unknown. */
+@property (nonatomic, readonly) NSString     *snapchatPhoneNumber;
+@property (nonatomic, readonly) NSDictionary *studySettings;
+/** Unknown. */
+@property (nonatomic, readonly) NSDictionary *targeting;
+@property (nonatomic, readonly) NSString     *userIdentifier;
+@property (nonatomic, readonly) BOOL         enableVideoFilters;
+@property (nonatomic, readonly) NSString     *QRPath;
+
+
+#pragma mark User preferences
+@property (nonatomic, readonly) BOOL           enableNotificationSounds;
+@property (nonatomic, readonly) NSUInteger     numberOfBestFriends;
+@property (nonatomic, readonly) SKStoryPrivacy storyPrivacy;
+@property (nonatomic, readonly) BOOL           isSearchableByPhoneNumber;
+@property (nonatomic, readonly) BOOL           privacyEveryone;
+
+#pragma mark Features
+@property (nonatomic, readonly) BOOL enableFrontFacingFlash;
+@property (nonatomic, readonly) BOOL enablePowerSaveMode;
+@property (nonatomic, readonly) BOOL enableReplaySnaps;
+@property (nonatomic, readonly) BOOL enableSmartFilters;
+@property (nonatomic, readonly) BOOL enableSpecialText;
+@property (nonatomic, readonly) BOOL enableSwipeCashMode;
+@property (nonatomic, readonly) BOOL enableVisualFilters;
 
 @end
--- SnapchatKit/Classes/Model/SKSession.m
+++ SnapchatKit/Classes/Model/SKSession.m
@@ -28,7 +28,9 @@
     
     NSDictionary *storiesResponse = json[@"stories_response"];
     NSDictionary *friendsResponse = json[@"friends_response"];
+    NSDictionary *updatesResponse = json[@"updates_response"];
     NSDictionary *identity        = json[@"identity_check_response"];
+    NSDictionary *features        = updatesResponse[@"feature_settings"];
     
     NSArray *friendStories = storiesResponse[@"friend_stories"];
     NSArray *myStories     = storiesResponse[@"my_stories"];
@@ -80,18 +82,78 @@
         
         // Group stories?
         _groupStories = @[];
+        
+        
+        
+        // Cash info
+        _canUseCash             = [updatesResponse[@"allowed_to_use_cash"] boolValue];
+        _isCashActive           = [updatesResponse[@"is_cash_active"] boolValue];
+        _cashCustomerIdentifier = updatesResponse[@"cash_customer_id"];
+        _cashClientProperties   = updatesResponse[@"client_properties"];
+        _cashProvider           = updatesResponse[@"cash_provider"];
+        
+        // Basic user info
+        _username     = updatesResponse[@"username"];
+        _email        = updatesResponse[@"email"];
+        _mobileNumber = updatesResponse[@"mobile"];
+        _recieved     = [updatesResponse[@"recieved"] integerValue];
+        _sent         = [updatesResponse[@"sent"] integerValue];
+        _score        = [updatesResponse[@"score"] integerValue];
+        _recents      = updatesResponse[@"recents"];
+        _requests     = updatesResponse[@"requests"];
+        
+        // Account information
+        _addedFriendsTimestamp      = [NSDate dateWithTimeIntervalSince1970:[updatesResponse[@"added_friends_timestamp"] doubleValue]/1000];
+        _authToken                  = updatesResponse[@"auth_token"];
+        _canSeeMatureContent        = [updatesResponse[@"can_view_mature_content"] boolValue];
+        _countryCode                = updatesResponse[@"country_code"];
+        _lastTimestamp              = [NSDate dateWithTimeIntervalSince1970:[updatesResponse[@"cash_provider"] doubleValue]/1000];
+        _devicetoken                = updatesResponse[@"device_token"];
+        _canSaveStoryToGallery      = [updatesResponse[@"enable_save_story_to_gallery"] boolValue];
+        _canVideoTranscodingAndroid = [updatesResponse[@"enable_video_transcoding_android"] boolValue];
+        _imageCaption               = [updatesResponse[@"image_caption"] boolValue];
+        _isTwoFAEnabled             = [updatesResponse[@"is_two_fa_enabled"] boolValue];
+        _lastAddressBookUpdateDate  = [NSDate dateWithTimeIntervalSince1970:[updatesResponse[@"last_address_book_updated_date"] doubleValue]/1000];
+        _lastReplayedSnapDate       = [NSDate dateWithTimeIntervalSince1970:[updatesResponse[@"last_replayed_snap_timestamp"] doubleValue]/1000];
+        _logged                     = [updatesResponse[@"logged"] boolValue];
+        _mobileVerificationKey      = updatesResponse[@"mobile_verification_key"];
+        _canUploadRawThumbnail      = [updatesResponse[@"raw_thumbnail_upload_enabled"] boolValue];
+        _seenTooltips               = updatesResponse[@"seen_tooltips"];
+        _shouldCallToVerifyNumber   = [updatesResponse[@"should_call_to_verify_number"] boolValue];
+        _shouldTextToVerifyNumber   = [updatesResponse[@"should_send_text_to_verify_number"] boolValue];
+        _snapchatPhoneNumber        = updatesResponse[@"snapchat_phone_number"];
+        _studySettings              = updatesResponse[@"study_settings"];
+        _targeting                  = updatesResponse[@"targeting"];
+        _userIdentifier             = updatesResponse[@"user_id"];
+        _enableVideoFilters         = [updatesResponse[@"video_filters_enabled"] boolValue];
+        _QRPath                     = updatesResponse[@"qr_path"];
+        
+        // Preferences
+        _enableNotificationSounds  = [updatesResponse[@"notification_sound_setting"] boolValue];
+        _numberOfBestFriends       = [updatesResponse[@"number_of_best_friends"] integerValue];
+        _privacyEveryone           = [updatesResponse[@"snap_p"] boolValue];
+        _isSearchableByPhoneNumber = [updatesResponse[@"searchable_by_phone_number"] boolValue];
+        _storyPrivacy              = SKStoryPrivacyFromString(updatesResponse[@"story_privacy"]);
+        
+        // Features
+        _enableFrontFacingFlash = [features[@"front_facing_flash"] boolValue];
+        _enablePowerSaveMode    = [features[@"power_save_mode"] boolValue];
+        _enableReplaySnaps      = [features[@"replay_snaps"] boolValue];
+        _enableSmartFilters     = [features[@"smart_filters"] boolValue];
+        _enableSpecialText      = [features[@"special_text"] boolValue];
+        _enableSwipeCashMode    = [features[@"swipe_cash_mode"] boolValue];
+        _enableVisualFilters    = [features[@"visual_filters"] boolValue];
     }
     
-    [self.knownJSONKeys addObjectsFromArray:@[@"stories_response", @"friends_response", @"identity_check_response", @"background_fetch_secret_key", @"discover"]];
+    [self.knownJSONKeys addObjectsFromArray:@[@"stories_response", @"friends_response", @"updates_response", @"identity_check_response", @"background_fetch_secret_key", @"discover"]];
     
     return self;
 }
 
 - (NSString *)description {
-    return [NSString stringWithFormat:@"<%@ stories delta=%hhd, discover=%hhd, email verified=%hhd, friends=%lu, added=%lu, stories=%lu, user stories=%lu>",
-            NSStringFromClass(self.class), self.storiesDelta, self.discoverSupported, self.emailVerified,
-            (unsigned long)self.friends.count, self.addedFriends.count, (unsigned long)self.stories.count,
-            (unsigned long)self.userStories.count];
+    return [NSString stringWithFormat:@"<%@ username=%@, mobile=%@, score=%lu, friends=%lu, added=%lu, stories=%lu, user stories=%lu>",
+            NSStringFromClass(self.class), self.username, self.mobileNumber, self.score, self.friends.count,
+            self.addedFriends.count, self.stories.count, self.userStories.count];
 }
 
 //- (id)initWithCoder:(NSCoder *)aDecoder {
