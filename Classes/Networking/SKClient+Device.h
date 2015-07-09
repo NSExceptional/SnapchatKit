9b0d638e24291ac0d66739114428f697c13d1fe8
ThePantsThief <tannerbennett@me.com>
2015-07-09 00:45:52 -0500

    Added documentation to SKClient+Device

--- Classes/Networking/SKClient+Device.h
+++ Classes/Networking/SKClient+Device.h
@@ -10,7 +10,11 @@
 
 @interface SKClient (Device)
 
+/** Sends the "app did open" event to Snapchat.
+ @param completion Takes an error, if any. */
 - (void)sendDidOpenAppEvent:(ErrorBlock)completion;
+/** Sends the "app did close" event to Snapchat.
+ @param completion Takes an error, if any. */
 - (void)sendDidCloseAppEvent:(ErrorBlock)completion;
 
 @end
