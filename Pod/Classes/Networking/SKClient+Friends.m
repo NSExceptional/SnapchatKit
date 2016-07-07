fce9ac41ff407b4280c607f5d8813495fcb8055b
Tanner Bennett <tannerbennett@me.com>
2016-07-07 15:19:45 -0500

    Update SKClient+Friends

--- Pod/Classes/Networking/SKClient+Friends.m
+++ Pod/Classes/Networking/SKClient+Friends.m
@@ -18,50 +18,51 @@
 
 @implementation SKClient (Friends)
 
-- (void)handleResponse:(id)object error:(NSError *)error completion:(ResponseBlock)completion {
-    NSDictionary *json = object;
-    if (error) {
-        completion(nil, error);
-    } else if ([json[@"logged"] boolValue]) {
-        SKUser *newFriend = [[SKUser alloc] initWithDictionary:json[@"object"]];
+- (void)handleResponse:(TBResponseParser *)parser completion:(ResponseBlock)completion {
+    if (parser.error) {
+        completion(nil, parser.error);
+    }
+    else if ([parser.JSON[@"logged"] boolValue]) {
+        SKUser *newFriend = [[SKUser alloc] initWithDictionary:parser.JSON[@"object"]];
         [self.currentSession.friends addObject:newFriend];
         completion(newFriend, nil);
-    } else if (json[@"message"]) {
-        completion(nil, [SKRequest errorWithMessage:json[@"message"] code:1]);
+    }
+    else if (parser.JSON[@"message"]) {
+        completion(nil, [TBResponseParser error:parser.JSON[@"message"] domain:@"SnapchatKit" code:parser.response.statusCode]);
     }
 }
 
 - (void)addFriend:(NSString *)username completion:(ResponseBlock)completion {
     NSParameterAssert(username);
-    NSDictionary *query = @{@"action": @"add",
-                            @"friend": username,
-                            @"username": self.currentSession.username,
-                            @"added_by": @"ADDED_BY_USERNAME"};
-    [self postTo:SKEPFriends.friend query:query callback:^(NSDictionary *json, NSError *error) {
-        [self handleResponse:json error:error completion:completion];
+    NSDictionary *params = @{@"action": @"add",
+                             @"friend": username,
+                             @"username": self.currentSession.username,
+                             @"added_by": @"ADDED_BY_USERNAME"};
+    [self postWith:params to:SKEPFriends.friend callback:^(TBResponseParser *parser) {
+        [self handleResponse:parser completion:completion];
     }];
 }
 
 - (void)addFriendBack:(NSString *)username completion:(ResponseBlock)completion {
     NSParameterAssert(username);
-    NSDictionary *query = @{@"action": @"add",
-                            @"friend": username,
-                            @"username": self.currentSession.username,
-                            @"added_by": @"ADDED_BY_ADDED_ME_BACK"};
-    [self postTo:SKEPFriends.friend query:query callback:^(NSDictionary *json, NSError *error) {
-        [self handleResponse:json error:error completion:completion];
+    NSDictionary *params = @{@"action": @"add",
+                             @"friend": username,
+                             @"username": self.currentSession.username,
+                             @"added_by": @"ADDED_BY_ADDED_ME_BACK"};
+    [self postWith:params to:SKEPFriends.friend callback:^(TBResponseParser *parser) {
+        [self handleResponse:parser completion:completion];
     }];
 }
 
 - (void)unfriend:(NSString *)friend completion:(ResponseBlock)completion {
     NSParameterAssert(friend);
-    NSDictionary *query = @{@"action": @"delete",
-                            @"friend": friend,
-                            @"username": self.currentSession.username};
-    [self postTo:SKEPFriends.friend query:query callback:^(NSDictionary *json, NSError *error) {
-        [self handleResponse:json error:error completion:^(SKUser *oldFriend, NSError *error) {
+    NSDictionary *params = @{@"action": @"delete",
+                             @"friend": friend,
+                             @"username": self.currentSession.username};
+    [self postWith:params to:SKEPFriends.friend callback:^(TBResponseParser *parser) {
+        [self handleResponse:parser completion:^(SKUser *oldFriend, NSError *error) {
             [self.currentSession.friends removeObject:oldFriend];
-            completion(oldFriend, error);
+            TBRunBlockP(completion, oldFriend, error);
         }];
     }];
 }
@@ -71,27 +72,25 @@
     if (!toAdd) toAdd = @[];
     if (!toUnfriend) toUnfriend = @[];
     
-    NSDictionary *query = @{@"username": self.currentSession.username,
-                            @"action": @"multiadddelete",
-                            @"friend": @{@"friendsToAdd": toAdd,
-                                         @"friendsToDelete": toUnfriend}.JSONString,
-                            @"added_by": @"ADDED_BY_USERNAME"};
-    [self postTo:SKEPFriends.friend query:query callback:^(NSDictionary *json, NSError *error) {
-        if (!error) {
-//            BOOL success = [json[@"message"] isEqualToString:@"success"];
-            completion(nil);
-        } else {
-            completion(error);
-        }
+    NSDictionary *params = @{@"username": self.currentSession.username,
+                             @"action": @"multiadddelete",
+                             @"friend": @{@"friendsToAdd": toAdd,
+                                          @"friendsToDelete": toUnfriend}.JSONString,
+                             @"added_by": @"ADDED_BY_USERNAME"};
+    [self postWith:params to:SKEPFriends.friend callback:^(TBResponseParser *parser) {
+        //        BOOL success = [parser.JSON[@"message"] isEqualToString:@"success"];
+        TBRunBlockP(completion, parser.error);
     }];
 }
 
 - (void)bestFriendsOfUsers:(NSArray *)usernames completion:(DictionaryBlock)completion {
     NSParameterAssert(usernames);
-    NSDictionary *query = @{@"friend_usernames": usernames.JSONString,
-                            @"friend_user_ids": @"", // TODO: user ids
-                            @"username": self.currentSession.username};
-    [self postTo:SKEPFriends.bests query:query callback:completion];
+    NSDictionary *params = @{@"friend_usernames": usernames.JSONString,
+                             @"friend_user_ids": @"", // TODO: user ids
+                             @"username": self.currentSession.username};
+    [self postWith:params to:SKEPFriends.bests callback:^(TBResponseParser *parser) {
+        TBRunBlockP(completion, parser.JSON, parser.error);
+    }];
 }
 
 - (void)findFriends:(NSDictionary *)friends completion:(ArrayBlock)completion {
@@ -99,85 +98,76 @@
     if (self.currentSession.shouldTextToVerifyNumber || self.currentSession.shouldCallToVerifyNumber)
         completion(nil, [SKRequest errorWithMessage:@"You need to verify your phone number first." code:1]);
     
-//    NSArray *findRequests = [friends split:30];
+    //    NSArray *findRequests = [friends split:30];
     
     // The people over at SnAPI say it only takes up to 30 numbers at a time,
     // but my phone sent way more than 30 at once.
-//    for (NSDictionary *find in findRequests) {
-        NSDictionary *query = @{@"username": self.username,
-                                @"countryCode": self.currentSession.countryCode,
-                                @"numbers": friends.JSONString};
-        [self postTo:SKEPFriends.find query:query callback:^(NSDictionary *json, NSError *error) {
-            if (!error) {
-                NSArray *contacts = json[@"results"];
-                NSMutableArray *foundFriends = [NSMutableArray array];
-                for (NSDictionary *f in contacts)
-                    [foundFriends addObject:[[SKFoundFriend alloc] initWithDictionary:f]];
-                completion(foundFriends, nil);
-            } else {
-                completion(nil, error);
-            }
-        }];
-//    }
+    //    for (NSDictionary *find in findRequests) {
+    NSDictionary *params = @{@"username": self.username,
+                             @"countryCode": self.currentSession.countryCode,
+                             @"numbers": friends.JSONString};
+    [self postWith:params to:SKEPFriends.find callback:^(TBResponseParser *parser) {
+        if (!parser.error) {
+            completion([SKThing transformJSONArray:parser.JSON[@"results"] toModelsOfClass:[SKFoundFriend class]], nil);
+        } else {
+            completion(nil, parser.error);
+        }
+    }];
 }
 
 - (void)findFriendsNear:(CLLocation *)location accuracy:(CGFloat)meters pollDurationSoFar:(NSUInteger)milliseconds completion:(ArrayBlock)completion {
     NSParameterAssert(location); NSParameterAssert(completion);
-    if (meters <= 0)
+    if (meters <= 0) {
         meters = 10;
+    }
     
-    NSDictionary *query = @{@"username": self.username,
-                            @"accuracyMeters": @(meters),
-                            @"action": @"update",
-                            @"lat": @(location.coordinate.latitude),
-                            @"long": @(location.coordinate.longitude),
-                            @"totalPollingDurationMillis": @(milliseconds)};
-    [self postTo:SKEPFriends.findNearby query:query callback:^(NSDictionary *json, NSError *error) {
-        if (!error) {
-            NSMutableArray *nearby = [NSMutableArray array];
-            for (NSDictionary *user in json[@"nearby_snapchatters"])
-                [nearby addObject:[[SKNearbyUser alloc] initWithDictionary:user]];
-            completion(nearby.copy, nil);
+    NSDictionary *params = @{@"username": self.username,
+                             @"accuracyMeters": @(meters),
+                             @"action": @"update",
+                             @"lat": @(location.coordinate.latitude),
+                             @"long": @(location.coordinate.longitude),
+                             @"totalPollingDurationMillis": @(milliseconds)};
+    [self postWith:params to:SKEPFriends.findNearby callback:^(TBResponseParser *parser) {
+        if (!parser.error) {
+            completion([SKThing transformJSONArray:parser.JSON[@"nearby_snapchatters"] toModelsOfClass:[SKNearbyUser class]], nil);
         } else {
-            completion(nil, error);
+            completion(nil, parser.error);
         }
     }];
 }
 
 - (void)searchFriend:(NSString *)query completion:(ResponseBlock)completion {
     NSParameterAssert(query.length); NSParameterAssert(completion);
-    [self postTo:SKEPFriends.search query:@{@"query": query, @"username": self.username} callback:completion];
+    [self postWith:@{@"query": query, @"username": self.username} to:SKEPFriends.search callback:^(TBResponseParser *parser) {
+        completion(parser.JSON ?: parser.data, parser.error);
+    }];
 }
 
 - (void)userExists:(NSString *)username completion:(BooleanBlock)completion {
     NSParameterAssert(username); NSParameterAssert(completion);
     
-    NSDictionary *query = @{@"request_username": username,
-                            @"username": self.username};
-    [self postTo:SKEPFriends.exists query:query callback:^(NSDictionary *json, NSError *error) {
-        if (!error) {
-            completion([json[@"exists"] boolValue], nil);
-        } else {
-            completion(NO, error);
-        }
+    NSDictionary *params = @{@"request_username": username,
+                             @"username": self.username};
+    [self postWith:params to:SKEPFriends.exists callback:^(TBResponseParser *parser) {
+        completion([parser.JSON[@"exists"] boolValue], parser.error);
     }];
 }
 
 - (void)updateDisplayNameForUser:(NSString *)friend newName:(NSString *)displayName completion:(ErrorBlock)completion {
     NSParameterAssert(displayName.length); NSParameterAssert(friend.length);
     
-    NSDictionary *query = @{@"action": @"display",
-                            @"display": displayName,
-                            @"friend": friend,
-                            @"friend_id": @"",
-                            @"username": self.username};
-    [self postTo:SKEPFriends.friend query:query callback:^(NSDictionary *json, NSError *error) {
-        if (!error) {
-            SKUser *updated = [[SKUser alloc] initWithDictionary:json[@"object"]];
+    NSDictionary *params = @{@"action": @"display",
+                             @"display": displayName,
+                             @"friend": friend,
+                             @"friend_id": @"",
+                             @"username": self.username};
+    [self postWith:params to:SKEPFriends.friend callback:^(TBResponseParser *parser) {
+        if (!parser.error) {
+            SKUser *updated = [[SKUser alloc] initWithDictionary:parser.JSON[@"object"]];
             [self.currentSession.friends removeObject:updated];
             [self.currentSession.friends addObject:updated];
         }
-        completion(error);
+        completion(parser.error);
     }];
 }
 
@@ -186,13 +176,13 @@
 }
 
 - (void)getSuggestedFriends:(ArrayBlock)completion {
-    NSDictionary *query = @{@"action": @"list",
-                            @"username": self.username};
-    [self postTo:SKEPMisc.suggestFriend query:query callback:^(NSDictionary *json, NSError *error) {
-        if (!error) {
-            completion([SKThing transformJSONArray:json[@"suggested_friend_results"] toModelsOfClass:nil], nil);
+    NSDictionary *params = @{@"action": @"list",
+                             @"username": self.username};
+    [self postWith:params to:SKEPMisc.suggestFriend callback:^(TBResponseParser *parser) {
+        if (!parser.error) {
+            completion([SKThing transformJSONArray:parser.JSON[@"suggested_friend_results"] toModelsOfClass:nil], nil);
         } else {
-            completion(nil, error);
+            completion(nil, parser.error);
         }
     }];
 }
@@ -200,19 +190,19 @@
 - (void)seenSuggestedFriends:(NSArray *)usernames seen:(BOOL)seen completion:(ErrorBlock)completion {
     if (!usernames.count) usernames = @[];
     
-    NSDictionary *query = @{@"action": @"update",
-                            @"seen": @(seen),
-                            @"seen_suggested_friend_list": usernames.JSONString,
-                            @"username": self.username};
-    [self postTo:SKEPMisc.suggestFriend query:query callback:^(NSDictionary *json, NSError *error) {
-        if (!error) {
-            BOOL success = [json[@"logged"] boolValue];
-            if (success)
+    NSDictionary *params = @{@"action": @"update",
+                             @"seen": @(seen),
+                             @"seen_suggested_friend_list": usernames.JSONString,
+                             @"username": self.username};
+    [self postWith:params to:SKEPMisc.suggestFriend callback:^(TBResponseParser *parser) {
+        if (!parser.error) {
+            if ([parser.JSON[@"logged"] boolValue]) {
                 completion(nil);
-            else
-                completion([SKRequest errorWithMessage:json[@"message"] code:1]);
+            } else {
+                completion([TBResponseParser error:parser.JSON[@"message"] domain:@"SnapchatKit" code:parser.response.statusCode]);
+            }
         } else {
-            completion(error);
+            completion(parser.error);
         }
     }];
 }
@@ -223,14 +213,13 @@
 
 - (void)setUserBlocked:(BOOL)blocked user:(NSString *)username reason:(SKBlockReason)reason completion:(ErrorBlock)completion {
     NSParameterAssert(username); NSParameterAssert(reason || !blocked);
-    
-    NSMutableDictionary *query = @{@"action": blocked ? @"block" : @"unblock",
-                                   @"friend": username,
-                                   @"friend_id": [self.currentSession userWithUsername:username].userIdentifier,
-                                   @"username": self.username}.mutableCopy;
-    if (blocked) query[@"block_reason_id"] = @(reason);
-    [self postTo:SKEPFriends.friend query:query callback:^(NSDictionary *json, NSError *error) {
-        completion(error);
+    NSMutableDictionary *params = @{@"action": blocked ? @"block" : @"unblock",
+                                    @"friend": username,
+                                    @"friend_id": [self.currentSession userWithUsername:username].userIdentifier,
+                                    @"username": self.username}.mutableCopy;
+    if (blocked) params[@"block_reason_id"] = @(reason);
+    [self postWith:params to:SKEPFriends.friend callback:^(TBResponseParser *parser) {
+        completion(parser.error);
     }];
 }
 
